1f98e260ee2524c023fd026cd55725a9b506635c
Nicolai Spohrer <nicolai@xeve.de>
2017-12-02 18:46:18 +0100

    Semi-working state

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -5,23 +5,116 @@
 // PI controller
 #include <PID_v1.h>
 
-#define DS18B20_PIN 5
+#define DS18B20_PIN 9
 
 OneWire oneWire(DS18B20_PIN);
 DallasTemperature tempSensor(&oneWire);
 
+double fTempSet = 65, fTempActual;
+double fPidOutput; // 0-255
+
+// PID controller parameters
+#define PID_kP 25.5
+#define PID_kI 1
+#define PID_kD 0
+
+PID oPID(&fTempActual, &fPidOutput, &fTempSet, PID_kP, PID_kI, PID_kD, DIRECT);
+
+// display
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+// Servo
+#include <Servo.h>
+#define SERVO_MIN_DEG 0
+#define SERVO_MAX_DEG 175
+#define SERVO_PIN 5
+Servo servo;
+
+
 void setup() {
+  
   Serial.begin(115200);
   tempSensor.begin();
+
+  tempSensor.requestTemperatures();
+  fTempActual = tempSensor.getTempCByIndex(0);
+  oPID.SetMode(AUTOMATIC);
+
+  servo.attach(SERVO_PIN);
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
+  display.display();
+  
+  display.clearDisplay();     
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+
+
+
+}
+
+double get_knob_pos(double fIntensity)
+{
+  // PID output value to servo position
+  // intensity from 0 - 255
+  if(fIntensity < 0 || fIntensity > 255)
+  { Serial.println("Illegal intensity value"); }
+
+  double fServoPos = map(fIntensity, 0, 255, SERVO_MAX_DEG, SERVO_MIN_DEG);
+
+  
+  return fServoPos;
+  
 }
 
 void loop() {
-    Serial.print("Requesting temperatures...");
-  tempSensor.requestTemperatures(); // Send the command to get temperatures
-  Serial.println("DONE");
-  // After we got the temperatures, we can print them here.
-  // We use the function ByIndex, and as an example get the temperature from the first sensor only.
-  Serial.print("Temperature for the device 1 (index 0) is: ");
-  Serial.println(tempSensor.getTempCByIndex(0));  
+  tempSensor.requestTemperatures();
+  fTempActual = tempSensor.getTempCByIndex(0);
+  fTempActual = 63;
+  oPID.Compute();
+
+  double fServoPos = get_knob_pos(fPidOutput);
+  servo.write(fServoPos);
+
+  Serial.print("Current temperature: ");
+  Serial.println(fTempActual);  
+  Serial.print("Set temperature: ");
+  Serial.println(fTempSet);  
+  Serial.print("PID output: ");
+  Serial.println(fPidOutput);
+  Serial.print("Knob pos: ");
+  Serial.println(fServoPos);
+
+
+  display.clearDisplay();
+  
+  display.setCursor(0,8*0); 
+  display.print("Cur temp: ");
+  display.print(fTempActual);
+  display.print(" degC");
+
+  display.setCursor(0, 8*1);
+  display.print("Set temp: ");
+  display.print(fTempSet);
+  display.print(" degC");
+
+  display.setCursor(0, 8*2);
+  display.print("PID output: ");
+  display.print(fPidOutput);
+
+  display.setCursor(0, 8*3);
+  display.print("Servo pos: ");
+  display.print(fServoPos);
+  
+  display.display();
+
+  Serial.println("---");
+  delay(1000);
 }
 
